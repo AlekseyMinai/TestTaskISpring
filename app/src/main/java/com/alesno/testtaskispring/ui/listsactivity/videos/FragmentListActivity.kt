685c421e7d70838c281bcaf85fb6173a5217cb5c
aleksei <AleksejZuka@gmail.com>
2019-11-22 01:20:17 +0300

    added change text and icon color in tabLayout

--- app/src/main/java/com/alesno/testtaskispring/ui/listsactivity/videos/FragmentListActivity.kt
+++ app/src/main/java/com/alesno/testtaskispring/ui/listsactivity/videos/FragmentListActivity.kt
@@ -2,8 +2,10 @@ package com.alesno.testtaskispring.ui.listsactivity.videos
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import com.alesno.testtaskispring.R
@@ -17,6 +19,7 @@ import com.alesno.testtaskispring.ui.listsactivity.videos.fragments.ListAllMovie
 import com.alesno.testtaskispring.ui.listsactivity.videos.fragments.ListFavoritMoviesFragment
 import com.alesno.testtaskispring.ui.listsactivity.videos.viewmodel.CommonViewModel
 import com.alesno.testtaskispring.ui.listsactivity.videos.viewmodel.CommonViewModelFactory
+import com.google.android.material.tabs.TabLayout
 import io.objectbox.Box
 import kotlinx.android.synthetic.main.activity_list_videos.*
 
@@ -62,13 +65,46 @@ class FragmentListActivity : AppCompatActivity() {
 
     private fun setupTabs(){
         setupTab(R.drawable.ic_movie_white_24dp, resources.getString(R.string.library), 0)
-        setupTab(R.drawable.ic_favorite_white_24dp, resources.getString(R.string.favorite), 1)
+        setupTab(R.drawable.ic_favorite_grey_24dp, resources.getString(R.string.favorite), 1)
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+                val tabPosition = p0!!.position
+                val tab: TextView = tab_layout.getTabAt(tabPosition)!!.customView as TextView
+                when(tabPosition){
+                    0 -> changeIconInTab(tab, R.drawable.ic_movie_grey_24dp)
+                    1 -> changeIconInTab(tab, R.drawable.ic_favorite_grey_24dp)
+                }
+                tab.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey))
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                val tabPosition = p0!!.position
+                val tab: TextView = tab_layout.getTabAt(tabPosition)!!.customView as TextView
+                when(tabPosition){
+                    0 -> changeIconInTab(tab, R.drawable.ic_movie_white_24dp)
+                    1 -> changeIconInTab(tab, R.drawable.ic_favorite_white_24dp)
+                }
+                tab.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorWhite))
+            }
+        })
+    }
+
+    private fun changeIconInTab(tab: TextView, iconResource: Int){
+        tab.setCompoundDrawablesWithIntrinsicBounds(0,
+            iconResource, 0,0)
     }
 
     private fun setupTab(iconResource: Int, text: String, position: Int){
         val tab: TextView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null) as TextView
         tab.text = text
         tab.setCompoundDrawablesWithIntrinsicBounds(0, iconResource, 0,0)
+        when(position){
+            1 -> tab.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorGrey))
+        }
         tab_layout.getTabAt(position)?.customView = tab
     }
 }
